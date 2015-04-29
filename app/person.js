4b5045c06a8d548c0fba4fb751933b52990e758c
aniaw <ania_w@outlook.com>
2015-04-29 11:23:17 +0200

    standarization

--- app/person.js
+++ app/person.js
@@ -1,19 +1,18 @@
-var myApp = angular.module("myApp", []);
+var myApp = angular.module('myApp', []);
 
-myApp.directive("change", function ()
+myApp.directive('change', function ()
 {
     return {
         scope: {
             change: '='
-        },
-        link: function (scope, element)
+        }, link: function (scope, element)
         {
-            element.bind("dblclick", function ()
+            element.bind('dblclick', function ()
             {
                 scope.change.firstName = scope.change.firstName.toUpperCase();
                 scope.change.lastName = scope.change.lastName.toLowerCase();
                 scope.change.age = 18;
-            })
+            });
         }
     };
 });
--- test/spec/person.spec.js
+++ test/spec/person.spec.js
@@ -1,35 +1,37 @@
-describe("directives", function ()
+'use strict';
+
+describe('directives', function ()
 {
     var $scope;
     var change;
 
-    beforeEach(module("myApp"));
+    beforeEach(module('myApp'));
     beforeEach(inject(function ($compile, $rootScope)
     {
         $scope = $rootScope;
-        $scope.person = {firstName: "John", lastName: "Watson", age: 21, email: "john.watson@email.com"};
+        $scope.person = {firstName: 'John', lastName: 'Watson', age: 21, email: 'john.watson@email.com'};
         change = angular.element('<div change="person"></div>');
         change = $compile(change)($rootScope);
     }));
 
-    describe("change", function ()
+    describe('change', function ()
     {
-        it("should update firstName on double click", function ()
+        it('should update firstName on double click', function ()
         {
 
         });
 
-        it("should update lastName on double click", function ()
+        it('should update lastName on double click', function ()
         {
 
         });
 
-        it("should update age on double click", function ()
+        it('should update age on double click', function ()
         {
 
         });
 
-        it("should update person object on double click", function ()
+        it('should update person object on double click', function ()
         {
 
         });
