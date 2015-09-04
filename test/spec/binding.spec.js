6b0cb9eefbfaaf3c8d1c96b725fff2540d106b85
stychu <tomaszstysial@o2.pl>
2015-09-04 15:08:26 +0200

    DONE

--- test/spec/binding.spec.js
+++ test/spec/binding.spec.js
@@ -3,12 +3,25 @@ describe('binding', function ()
 {
     var text;
     var operation;
+    var $scope;
 
-    beforeEach(function ()
+    beforeEach(inject(function ($compile, $rootScope)
     {
+        $scope = $rootScope;
         text = angular.element('<div>{{"Ala" +  " has " + 3 + " cats"}}</div>');
         operation = angular.element('<div>{{111 * 111}}</div>');
-
+        text = $compile(text)($rootScope);
+        operation = $compile(operation)($rootScope);
+    }));
+    
+    it ('Should return \'Ala has 3 cats\'', function () {
+        $scope.$digest();
+        expect(text.html()).toBe('Ala has 3 cats');
     });
+
+    it ('Should return 12321', function () {
+        $scope.$digest();
+        expect(operation.html()).toBe('12321');
+    })
 
 });
