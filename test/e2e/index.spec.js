9ebacb0a76a0c9b503d36a696a4031984248a4f8
stychu <tomaszstysial@o2.pl>
2015-09-22 11:12:38 +0200

    DONE

--- test/e2e/index.spec.js
+++ test/e2e/index.spec.js
@@ -2,20 +2,33 @@ describe("weatherApp", function ()
 {
     describe("index", function ()
     {
+        beforeEach(function () {
+            browser.get('/#');
+        });
+
         it("should display the correct title", function ()
         {
-            browser.get('/#');
             expect(browser.getTitle()).toBe('AngularJS Tutorials');
         });
 
         it("should display the increase temperature when plus button clicked", function ()
         {
+            var button = element(by.id('upButton')),
+                message = element(by.binding('currentTemp'));
 
+            button.click();
+
+            expect(message.getText()).toBe('30 °C');
         });
 
         it("should display the increase temperature when minus button clicked", function ()
         {
+            var button = element(by.id('downButton')),
+                message = element(by.binding('currentTemp'));
 
+            button.click();
+
+            expect(message.getText()).toBe('28 °C');
         });
     });
 });
